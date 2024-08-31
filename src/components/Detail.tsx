3450ab46fbecca236c0516b15fb8005ef257f38f
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-08-31 08:01:35 +0700

    2!

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -4,14 +4,16 @@ import { LangContext } from "@contexts/LangContext";
 
 export const Detail = () => {
   const { theme } = useContext(ThemeContext);
+  const { lang } = useContext(LangContext);
   return (
     <div>
       <p className="text-center" style={{ color: theme.fgColor }}>
-        Display activated theme message here (EN or TH)
         {/* tip: Get information from "theme" variable to determine 
     which theme is currently selected */}
         if(  theme.name === "dark"){
-          
+          lang.detail.darkActivated
+        }else if (theme.name === "light" ){
+          lang.detail.lightActivated
         }
       </p>
     </div>
